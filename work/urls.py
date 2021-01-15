061eec197eec1c332ecff08774125e07281f44cd
Neto42 <Neto42@users.noreply.github.com>
2021-01-15 23:22:32 +0300

    media

--- work/urls.py
+++ work/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -8,4 +10,4 @@ urlpatterns = [
     path('fwork/', include('hh_site.urls')),
     path('faq/', include('faq.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
